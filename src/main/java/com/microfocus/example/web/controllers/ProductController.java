5a9687acb3691214b95329448f2c3baa703c4a03
Veerapuram Varadhan <varadhan.veerapuram@microfocus.com>
2022-07-30 01:06:40 +0530

    A03:2021 Injection - Cross site scripting (Reflected) vulnerability added

--- src/main/java/com/microfocus/example/web/controllers/ProductController.java
+++ src/main/java/com/microfocus/example/web/controllers/ProductController.java
@@ -38,6 +38,7 @@ import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -78,6 +79,15 @@ public class ProductController extends AbstractBaseController {
     @Override
     String GetControllerName() {
         return CONTROLLER_NAME;
+    }
+    
+    @GetMapping("/xss")
+    @ResponseBody
+    public ResponseEntity<String> getKeywordsContent(@Param("keywords") String keywords) {
+
+    	String retContent = "Product search using: " + keywords;
+    	
+        return ResponseEntity.ok().body(retContent);
     }
 
     @GetMapping(value = {"", "/"})
