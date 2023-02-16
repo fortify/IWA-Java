3dd36b97b8ed2e02d714c8d913ef29fef2f9964b
Rohit Baryha <72431329+rohitbaryha1@users.noreply.github.com>
2023-02-16 10:44:01 +0530

    Review comments incorporated
    FortifyScanCommands.bat will be deleted
    AWS script is align with bash file
    GCP keep it as it.

--- FortifyScanCommands.bat
+++
@@ -1,7 +0,0 @@
-Del *.fpr
-del *.log
-mvn clean package
-sourceanalyzer -b iwa -clean
-sourceanalyzer -b iwa -cp "**/*" "**/*" -source 11 -sql-language PL/SQL -debug -logfile trans.log -verbose
-sourceanalyzer -b iwa -debug -logfile scan.log -scan -f iwa.fpr -Dcom.fortify.sca.rules.enable_wi_correlation
-start "" "iwa.fpr"
