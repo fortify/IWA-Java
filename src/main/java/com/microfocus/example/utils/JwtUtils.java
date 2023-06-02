0a6003e1be788964be31357ea2313c8c1074ba7c
kadraman <kevin.lee@microfocus.com>
2023-06-02 09:24:16 +0100

    Updated copyright statement for changed files to Open Text.

--- src/main/java/com/microfocus/example/utils/JwtUtils.java
+++ src/main/java/com/microfocus/example/utils/JwtUtils.java
@@ -1,7 +1,7 @@
 /*
         Insecure Web App (IWA)
 
-        Copyright (C) 2020-2022 Micro Focus or one of its affiliates
+        Copyright 2020-2023 Open Text or one of its affiliates.
 
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
